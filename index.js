a17a4799f253dd7d600756ab2604bc1a765e93e4
sleeplesseditor <josh@bryson.me.uk>
2022-02-27 23:28:48 +0000

    Main file structure setup

---
+++ index.js
@@ -0,0 +1,3 @@
+require('./sockets/socketMain');
+require('./expressFiles/expressMain');
+
--- servers.js
+++ servers.js
@@ -4,5 +4,12 @@ app.use(express.static(__dirname + '/public'));
 const socketio = require('socket.io');
 const expressServer = app.listen(8080);
 const io = socketio(expressServer);
+const helmet = require('helmet');
+app.use(helmet());
 
 console.log('Server is listening on Port 8080')
+
+module.exports = {
+    app,
+    io
+}
